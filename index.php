17a48ddc76c8989fe5342036e874d244380b01bf
Nikolay Lamanov <59505751+hard-it@users.noreply.github.com>
2020-02-25 15:33:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <? if(!empty($arResult["PROPERTIES"]['DOCS']['VALUE'])){ ?>
 <h3 class="h3-recom">Документы</h3>
 <div class="file-load">
-	<?foreach($arResult["PROPERTIES"]['DOCS']['VALUE'] as $pid=>$arProperty):?>
-	<? $data = CFile::GetFileArray($arProperty); ?>
-	<div class="file-load-item">
-		<div class="file-inf">
-			<a href="<?=$data['SRC']?>" download>
-				<span><?=$data['ORIGINAL_NAME']?></span>
-				<span><?=round($data['FILE_SIZE']/1024)?> кБ</span>
-			</a>
-		</div>
-	</div>
-	<?endforeach?>
+<?foreach($arResult["PROPERTIES"]['DOCS']['VALUE'] as $pid=>$arProperty):?>
+<? $data = CFile::GetFileArray($arProperty); ?>
+    <div class="file-load-item">
+	<div class="file-inf">
+	    <a href="<?=$data['SRC']?>" download><span><?=$data['ORIGINAL_NAME']?></span><span><?=round($data['FILE_SIZE']/1024)?> кБ</span></a>
+        </div>
+    </div>
+<?endforeach?>
 </div>
 <? } ?>
